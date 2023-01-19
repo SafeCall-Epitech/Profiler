17e583ee0c5f3d13752ebed2d1b5c6e6c53abe42
Julien Garsia <julien.garsia@epitech.eu>
2023-01-19 22:04:06 +0100

    fix

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@ apk update
 apk add pkgconfig libzmq zeromq-dev gcc musl-dev
 
 go mod download
+mv ../usr/lib/libzmq.so.5.2.4 .
+mv ../usr/lib/libzmq.so.5 .
+mv ../usr/lib/libzmq.so .
 go build -o profiler *.go
