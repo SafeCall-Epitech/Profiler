7523c8d07ac6e7be806c05cede86a8c364151a8c
Julien Garsia <julien.garsia@epitech.eu>
2023-09-14 18:27:28 +0200

    FIX : Mutilple friends acceptation

--- handlerFriends.go
+++ handlerFriends.go
@@ -18,7 +18,7 @@ func actionFriendHandler(userID, dest, action string) string {
 		addDelFriend(uri, dest, userID, "$pull")
 		addDelFriend(uri, userID, dest, "$pull")
 		addDelFriend(uri, userID, "?"+dest, "$pull")
-	} else if action == "accept" {
+	} else if action == "accept" && !check_duplicata(userID, dest) {
 		addDelFriend(uri, userID, "?"+dest, "$pull")
 		addDelFriend(uri, userID, dest, "$push")
 	} else if action == "deny" {
@@ -36,4 +36,14 @@ func getFriendsHandler(userID string) []string {
 	str := fmt.Sprintf("%v", result)
 	dest := strings.Split(str[1:len(str)-1], " ")
 	return dest
+}
+
+func check_duplicata(userID, NewFriend string) bool {
+	friends := getFriendsHandler(userID)
+	for _, friend := range friends {
+		if friend == NewFriend {
+			return true
+		}
+	}
+	return false
 }
